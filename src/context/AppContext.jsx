c8810d406013c7811bdb83b0e22a33854962416f
Jubair02 <104502849+Jubair02@users.noreply.github.com>
2026-01-12 18:12:36 +0600

    updated

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createContext } from "react";  
+
+
+ export const AppContext = createContext()
+   
+
+  export const AppContextProvider = ( props ) => { 
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const value = {
+      currency
+    } 
+    
+    return (
+      <AppContext.Provider value={value}>
+        {props.children}
+      </AppContext.Provider>
+    );
+  }
