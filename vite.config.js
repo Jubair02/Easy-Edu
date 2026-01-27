9b13ed88ef729a7c1f32028a43fdfa94e540a99f
Jubair02 <104502849+Jubair02@users.noreply.github.com>
2026-01-28 00:22:01 +0600

    funal

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/Easy-Education",
+  base: "/Easy-Edu",
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
